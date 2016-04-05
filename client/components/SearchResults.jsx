12c38fc5b7d749874dbc0945c28368c9e0147e8e
Benjamin Florin <benjamin.florin@gmail.com>
2016-04-05 18:32:14 -0400

    Added basic styling

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -2,15 +2,18 @@ import React, { PropTypes } from "react";
 
 export const SearchResults = ({hits, total, filters, removeFilter}) => {
 
-    let result_box = <div>You searched for:
-        {filters.map((filter)=> (<div>{filter}<a onClick={() => removeFilter(filter)}>x</a></div>))}
+    let result_box = <div className="searched-for-box">You searched for:
+        <ul>
+            {filters.map((filter)=> (<li className='searched-for-item'>
+                <div className="filter">{filter}</div>
+                <a className='remove-filter' onClick={() => removeFilter(filter)}>x</a></li>))}
+        </ul>
     </div>;
 
-    return <div>
-        <h2>Results</h2>
-
+    return <div className="search-result-page">
         {filters.length ? result_box : ''}
-        <ul>
+        <h2>Results</h2>
+        <ul id="search-results">
             {hits.map((hit) => (<li><a href={hit.id}>{hit.title}</a></li>))}
         </ul>
     </div>
