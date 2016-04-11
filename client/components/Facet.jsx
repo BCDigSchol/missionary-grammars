0a7ffc5069957ec2ee502d9be1eea604e510017b
Benjamin Florin <benjamin.florin@gmail.com>
2016-04-11 17:23:06 -0400

    Make facet labels clickable

--- client/components/Facet.jsx
+++ client/components/Facet.jsx
@@ -4,12 +4,11 @@ import { FacetItem } from "./FacetItem.jsx";
 
 export const Facet = ({addFilter, removeFilter, filters, values, displayName, field, open_fields, toggleField}) => {
     let toggle_switch = (
-        <div className="open-field-control"
-             onClick={() => toggleField(field)}>{open_fields.indexOf(field) > -1 ? '-' : '+'}</div>
+        <div >{open_fields.indexOf(field) > -1 ? '-' : '+'}</div>
     );
 
     return <div>
-        <h3>{displayName} {toggle_switch}</h3>
+        <h3 className="open-field-control" onClick={() => toggleField(field)}>{displayName} {toggle_switch}</h3>
         <ul className={open_fields.indexOf(field) > -1 ? 'show' : 'hidden'}>
             {values.map((value) => (<FacetItem value={value}
                                                addFilter={addFilter}
